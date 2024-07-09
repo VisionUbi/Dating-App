cbecccab1274ff96aa6a7e2c23929fae695434be
VisionUbi <ubaidk2016@gmail.com>
2024-07-09 11:47:52 +0500

    connection string and intial migrations

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcfa3eb91efa0978c84ffdccf69a72cd63ae2c35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+437be9cbf52957a4f3ca46057a56a49f6608c91a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
