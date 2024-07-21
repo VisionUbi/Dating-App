ca53f36f9d0a25c871e72bf9816acb24f7bdac24
VisionUbi <ubaidk2016@gmail.com>
2024-07-21 23:01:51 +0500

    angular

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  title = 'client';
+  http = inject(HttpClient);
+}
