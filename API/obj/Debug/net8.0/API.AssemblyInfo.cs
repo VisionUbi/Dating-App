7bded2c78b2d9390de13f2efdd2affb856016b23
Muhammad Ubaid khan <ubaid.khan@ext.axian-group.com>
2025-03-07 22:31:27 +0500

    ignored

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cea98eeba412812527fc020379332e0947f9e4d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+799eafb88d1cfaf16370ea7ca88284924101984e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
