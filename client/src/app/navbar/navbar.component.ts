3170ef8ce7c5e0dbc7cd39e70aa2d80d097142f9
Muhammad Ubaid khan <ubaid.khan@ext.axian-group.com>
2025-03-10 09:56:43 +0500

    routing

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,18 +1,20 @@
 import { Component, inject, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FormsModule,BsDropdownModule],
+  imports: [FormsModule,BsDropdownModule,RouterLink,RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],  
 })
 export class NavbarComponent {
   model: any = {}; 
   accountService = inject(AccountService);
+  router = inject(Router);
 
   constructor(@Inject(AccountService) private _accountService: AccountService) {} // Fixed Inject syntax
 
@@ -21,6 +23,7 @@ export class NavbarComponent {
     (
       {
       next: response => {
+        this.router.navigateByUrl('/members');
         console.log('Logged in successfully',response);
       },
       error: (error) => {
@@ -29,6 +32,7 @@ export class NavbarComponent {
   }
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
  
