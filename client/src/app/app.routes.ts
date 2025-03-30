7b0a6002510c51e4903eb225b124e6dbc9300eb8
Muhammad Ubaid khan <ubaid.khan@ext.axian-group.com>
2025-03-31 00:43:19 +0500

    create test error component to hit those apis to get error in console for now

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './_guards/auth.guard';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -12,5 +13,6 @@ export const routes: Routes = [
     { path: 'members/:id', component: MemberDetailComponent },
     { path: 'lists', component: ListsComponent },
     { path: 'messages', component: MessagesComponent },
+    { path: 'errors', component: TestErrorsComponent },
     { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
