c64d79b84068b153a48b33ccbc4ef23b499b174f
Muhammad Ubaid khan <ubaid.khan@ext.axian-group.com>
2025-03-30 18:13:07 +0500

    added swagger and error handler controller

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,39 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController(DataContext context) : BaseApiController
+    {
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetAuth()
+        {
+            return "Secret Text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = context.Users.Find(-1);
+            if (thing == null) return NotFound();
+            return Ok(thing);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<AppUser> GetServerError()
+        {
+            var thing = context.Users.Find(-1) ?? throw new System.Exception("A bad things happen");
+            return thing;
+        }
+
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not a good request");
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,41 @@
 using API.Data;
 using API.Extentions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityService(builder.Configuration);
 
 builder.Services.AddCors();
-var app = builder.Build();
- app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+builder.Services.AddControllers();
 
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My API",
+        Version = "v1",
+        Description = "An API for my application"
+    });
+});
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; // Set Swagger UI at the root URL
+});
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+ 
+ 
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
