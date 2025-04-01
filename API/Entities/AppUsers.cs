7ec817ca6243db5dde9651688374f60744b43d03
Muhammad Ubaid khan <ubaid.khan@ext.axian-group.com>
2025-04-01 17:03:05 +0500

    Section 8 : created all tables

--- API/Entities/AppUsers.cs
+++ API/Entities/AppUsers.cs
@@ -1,10 +1,24 @@
-﻿namespace API.Entities;
+﻿using Microsoft.AspNetCore.Identity;
 
-public class AppUser
+namespace API.Entities;
+
+public class AppUser : IdentityUser<int>
 {
-    public int Id { get; set; }
-    public required string UserName { get; set; }
-    public required byte[] PasswordHash { get; set; }
-    public required byte[] PasswordSalt { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public required string KnownAs { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public required string Gender { get; set; }
+    public string? Introduction { get; set; }
+    public string? Interests { get; set; }
+    public string? LookingFor { get; set; }
+    public required string City { get; set; }
+    public required string Country { get; set; }
+    public List<Photo> Photos { get; set; } = [];
+    public List<UserLike> LikedByUsers { get; set; } = [];
+    public List<UserLike> LikedUsers { get; set; } = [];
+    public List<Message> MessagesSent { get; set; } = [];
+    public List<Message> MessagesReceived { get; set; } = [];
+    public ICollection<AppUserRole> UserRoles { get; set; } = [];
 
 }
